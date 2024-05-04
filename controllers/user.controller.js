a65eeb58d2e749d518929814ddc828b0262acb12
gitpremsagar <psagar172@gmail.com>
2024-05-04 15:50:57 +0530

    feat - create user route works

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,10 +1,37 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 // import sequelize
 const { User } = require("../configs/db.js");
 
+// import jwt and bcrypt
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET_KEY;
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 const createUser = async (req, res) => {
   try {
+    // check if user already exists
+    const existing = await User.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (existing) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+
+    // hash the password
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+
+    // replace the password with the hashed password
+    req.body.password = hashedPassword;
+
     // create a new user
     const user = await User.create(req.body);
+
+    // remove the password from the response
+    user.password = undefined;
 
     // send response
     res.status(201).json(user);
