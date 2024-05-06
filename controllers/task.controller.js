99fb06abd267dd767b1d096799420f3b2f0626eb
gitpremsagar <psagar172@gmail.com>
2024-05-06 10:42:43 +0530

    fix - saves userId instead of first name and last name for assignedTo value

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -1,22 +1,8 @@
 const { Task, User, Project } = require("../configs/db");
 
 const createTask = async (req, res) => {
-  //   taskId int(11) AI PK
-  // projectName varchar(255)
-  // projectId int(11)
-  // taskName varchar(255)
-  // taskDescription varchar(255)
-  // spendTime varchar(255)
-  // priority varchar(255)
-  // assignedTo varchar(255)
-  // status varchar(255)
-  // createdBy varchar(255)
-  // updatedBy varchar(255)
-  // createdAt datetime
-  // updatedAt datetime
   try {
     const project = await Project.findByPk(req.body.projectId);
-    const user = await User.findByPk(req.body.assignedTo);
     const task = await Task.create({
       projectName: project.projectName,
       projectId: req.body.projectId,
@@ -24,7 +10,7 @@ const createTask = async (req, res) => {
       taskDescription: req.body.taskDescription,
       spendTime: req.body.spendTime,
       priority: req.body.priority,
-      assignedTo: `${user.firstName} ${user.lastName}`,
+      assignedTo: req.body.assignedTo,
       status: req.body.status,
       createdBy: `${req.user.firstName} ${req.user.lastName}`,
       updatedBy: `${req.user.firstName} ${req.user.lastName}`,
@@ -90,14 +76,27 @@ const getTasksByProjectId = async (req, res) => {
 
 const updateTaskById = async (req, res) => {
   try {
+    const project = await Project.findByPk(req.body.projectId);
     const task = await Task.findByPk(req.params.taskId);
     if (task) {
-      task.update(req.body);
+      task.update({
+        projectName: project.projectName,
+        projectId: req.body.projectId,
+        taskName: req.body.taskName,
+        taskDescription: req.body.taskDescription,
+        spendTime: req.body.spendTime,
+        priority: req.body.priority,
+        assignedTo: req.body.assignedTo,
+        status: req.body.status,
+        createdBy: `${req.user.firstName} ${req.user.lastName}`,
+        updatedBy: `${req.user.firstName} ${req.user.lastName}`,
+      });
       res.status(200).json(task);
     } else {
       res.status(404).json({ message: "Task not found" });
     }
   } catch (error) {
+    console.log("Error updating task", error);
     res.status(500).json({ error: error.message });
   }
 };
